ab3a500cfbda00f86a312bd7517d163f2ec9f1a1
RenilsonAlbuquerque <renilsonalbuquerque@gmail.com>
2019-04-24 09:22:16 -0300

    Initial commit

---
+++ moviehub/src/main/java/br/pitang/moviehub/models/Movie.java
@@ -0,0 +1,4 @@
+package br.pitang.moviehub.models;
+
+public class Movie {
+}
---
+++ moviehub/src/test/java/br/pitang/moviehub/MoviehubApplicationTests.java
@@ -0,0 +1,16 @@
+package br.pitang.moviehub;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MoviehubApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
