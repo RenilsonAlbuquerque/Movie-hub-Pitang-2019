3e112ee48253a4ef54301100d843043cd8cb9ab5
RenilsonAlbuquerque <renilsonalbuquerque@gmail.com>
2019-05-08 14:32:03 -0300

    Navbar and menus

--- movieHub-angular/src/app/app-routing.module.ts
+++ movieHub-angular/src/app/app-routing.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [
+
   {
     path:'',
-    redirectTo: 'movie',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
-    path: 'movie',
-    loadChildren: './movie/movie.module#MovieModule'
+    path: 'home',
+    component: LayoutComponent,
+    children:[
+      {
+        path:'',
+        redirectTo: 'movie',
+        pathMatch: 'full'
+      },
+      {
+        path: 'movie',
+        loadChildren: './movie/movie.module#MovieModule'
+      }
+    ]
   }
+  
 ];
 
 @NgModule({
--- movieHub-angular/src/app/app.module.ts
+++ movieHub-angular/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
